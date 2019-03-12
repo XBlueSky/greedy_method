2d3da32947d7d2c7f7e324effdee3dc22dfcef16
XBlueSky <tony84822@gmail.com>
2019-03-12 14:13:19 +0800

    form energy type vehicular-fog

--- script/create_fog.py
+++ script/create_fog.py
@@ -7,8 +7,8 @@ parser.add_argument("fog_num", help="Input number of fogs")
 parser.add_argument("cost", help="Input fixed cost number or diff")
 args = parser.parse_args()
 
-for i in range(1):
-    with open("../testcase/Sfog_" + args.fog_num + "_v" + str(i+1), 'w') as fp:
+for i in range(10):
+    with open("../testcase/Efog_" + args.fog_num + "_v" + str(i+1), 'w') as fp:
 
         for i in range(int(args.fog_num)):
             if i < 3 :
@@ -21,14 +21,15 @@ for i in range(1):
             # cost
             if args.cost == "diff":
                 for v in range(vehicle_num):
-                    if i == 9:
-                        diff_cost = random.randint(5, 50)
-                    elif i % 3 == 0:
-                        diff_cost = random.randint(1, 10)
-                    elif i % 3 == 1:
-                        diff_cost = random.randint(20, 30)
-                    elif i % 3 == 2:
-                        diff_cost = random.randint(40, 50)
+                    # if i == 9:
+                    #     diff_cost = random.randint(5, 50)
+                    # elif i % 3 == 0:
+                    #     diff_cost = random.randint(1, 10)
+                    # elif i % 3 == 1:
+                    #     diff_cost = random.randint(20, 30)
+                    # elif i % 3 == 2:
+                    #     diff_cost = random.randint(40, 50)
+                    diff_cost = random.randint(5, 50)
                         
                     fp.write(str(diff_cost) + " ")
             else:
@@ -38,7 +39,8 @@ for i in range(1):
 
             # consumptoin rate
             for v in range(vehicle_num):
-                consumption_rate = random.randint(1, 9)
+                # consumption_rate = random.randint(1, 9)
+                consumption_rate = 5
                 fp.write(str(consumption_rate) + " ")
             fp.write("\n")
 
@@ -47,11 +49,23 @@ for i in range(1):
             threshold_power     = []
             for v in range(vehicle_num):
                 if i == 9:
-                    initial = random.randint(30, 40)
-                    threshold = random.randint(30, 35)
-                else:
                     initial = random.randint(30, 100)
                     threshold = random.randint(10, 50)
+                elif i % 3 == 0:
+                    initial = random.randint(30, 40)
+                    threshold = random.randint(30, 35)
+                elif i % 3 == 1:
+                    initial = random.randint(50, 70)
+                    threshold = random.randint(30, 35)
+                elif i % 3 == 2:
+                    initial = random.randint(80, 100)
+                    threshold = random.randint(30, 35)
+                # if i == 9:
+                #     initial = random.randint(30, 40)
+                #     threshold = random.randint(30, 35)
+                # else:
+                #     initial = random.randint(30, 100)
+                #     threshold = random.randint(10, 50)
                 if initial < threshold:
                     initial_power.append(threshold)
                     threshold_power.append(initial)
